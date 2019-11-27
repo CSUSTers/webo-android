8fc3dc27e6a31d7900f478760769971a763227da
icceey <iceclock1037@gmail.com>
2019-11-27 19:37:22 +0800

    ???

---
+++ app/src/main/java/net/csust/webo/BaseActivity.java
@@ -0,0 +1,14 @@
+package net.csust.webo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/net/csust/webo/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package net.csust.webo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import net.csust.webo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
