c46caeaf9a8621bbdcb40e8e8c1e9f2f67c2e646
icceey <iceclock1037@gmail.com>
2019-12-03 12:05:06 +0800

    change to flutter

---
+++ android/app/src/main/kotlin/net/csust/webo/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.csust.webo
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
